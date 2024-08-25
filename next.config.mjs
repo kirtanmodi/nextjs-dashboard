a47d8798add423e19edcdbd50b5f5e180e1eb384
Kirtan Modi <modikirtan19@gmail.com>
2024-08-26 03:13:50 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   experimental: {
     ppr: "incremental",
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://www.next.modikirtan.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
